e36b62d1c21bf27447cba21ffba0a6a8c7d877b9
Anita Growsoon <anitagrowsoon@gmail.com>
2015-12-22 17:27:43 -0500

    implementing jquery fullpage plugin

---
+++ js/script.js
@@ -0,0 +1,24 @@
+if ($(window).width() > 1000) {
+   $(function () {
+
+      $('#fullpage').fullpage({
+         anchors:['main', 'about', 'port', 'charity', 'contact'],
+         navigation: true,
+         navigationPosition: 'right',
+      });
+
+   });
+}
+
+if ($(window).width() < 999) {
+   $(function () {
+
+     $('#fullpage').fullpage({
+         anchors:['main', 'about', 'port', 'charity', 'contact'],
+         navigation: true,
+         navigationPosition: 'right',
+         scrollBar: true
+      });
+
+   });
+}
